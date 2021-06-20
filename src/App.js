51de848267a7dd4b273f1eb964120a7b8c6d4777
Meshari Aldoukhi <mashry151@hotmail.com>
2021-06-20 17:09:51 +0300

    Clean the Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import './App.css';
+import "./App.css";
 /* Component */
-import Grid from './components/Grid';
-import { Header, StartBtn, Note } from './styles';
-import {useState} from 'react'
+import Grid from "./components/Grid";
+/* Styled-component */
+import { Header, StartBtn, Note } from "./styles";
+/* Global react */
+import { useState } from "react";
 
 function App() {
-  const [view, setView] = useState(false)
-  const handleClick =  () => {
-    setView(true)
-  }
+  /* State to start the game */
+  const [view, setView] = useState(false);
+  /* Function will set the view to true  */
+  const handleClick = () => {
+    setView(true);
+  };
+  /* Function to Show the message or the cards */
   const show = () => {
-    return view ? <Grid /> : <Note>Please Click Button To start the game, and notice when you click the button timer will start count</Note>
-  }
+    return view ? (
+      <Grid />
+    ) : (
+      <Note>
+        Please Click Button To start the game, and notice when you click the
+        button timer will start count
+      </Note>
+    );
+  };
   return (
-      <div>
-        <Header>The Memory Game</Header>
-        <StartBtn onClick={handleClick} className={view}><a></a></StartBtn>
-        {show()}
-      </div>
+    <div>
+      <Header>The Memory Game</Header>
+      <StartBtn onClick={handleClick} className={view}>
+        <a></a>
+      </StartBtn>
+      {show()}
+    </div>
   );
 }
 
